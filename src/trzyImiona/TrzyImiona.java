75c90426c413ee3ccd5509d20f2a23f5dd2199e0
Wojciech T <hadwao2@gmail.com>
2018-09-22 20:59:45 +0200

    5.1

---
+++ src/trzyImiona/TrzyImiona.java
@@ -0,0 +1,13 @@
+package trzyImiona;
+
+import java.util.Arrays;
+
+public class TrzyImiona {
+
+    public static void main(String[] args) {
+        String[] trzyImiona = new String[]{"Jan", "Satnisław", "Wiesław"};
+
+        System.out.println(Arrays.toString(trzyImiona));
+
+    }
+}
